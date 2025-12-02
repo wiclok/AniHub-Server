7106a43d91ad30995d44bdb41c903e373852b710
Your Name <you@example.com>
2025-12-02 17:33:31 -0300

    feat(anime): add module with controller, service and initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MailerModule } from './mailer/mailer.module';
+import { AnimeModule } from './anime/anime.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule, MailerModule],
+  imports: [PrismaModule, AuthModule, UserModule, MailerModule, AnimeModule],
   controllers: [AppController],
   providers: [AppService],
 })
